d7e079eee121824ed8dc6015164b6c6e51dc112a
Bohdan <yulia.pakhachuk@gmail.com>
2023-04-27 17:33:24 +0200

    used property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { WishCardComponent } from './wish-card/wish-card.component';
-import { GenerateBtnComponent } from './generate-btn/generate-btn.component';
+import { PredictCardComponent } from './predict-card/predict-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    WishCardComponent,
-    GenerateBtnComponent
+    PredictCardComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/predict-card/predict-card.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-predict-card',
+  templateUrl: './predict-card.component.html',
+  styleUrls: ['./predict-card.component.css'],
+})
+export class PredictCardComponent {
+  predictText: string = 'My predict is ...';
+  generatePrediction() {
+    const predictions = [
+      'Some days you are pigeon, some days you are statue. Today, bring umbrella.',
+      'Your reality check about to bounce.',
+      'Wise person never try to get even. Wise person get odder.',
+      'Two days from now, tomorrow will be yesterday.',
+      'Your inferiority complex not good enough. Try harder.',
+      "When chosen for jury duty, tell judge 'fortune cookie say guilty!'",
+      'Stop eating now. Food poisoning no fun.',
+      'You are cleverly disguised as responsible adult.',
+      'Tomorrow at breakfast, listen carefully: do what rice krispies tell you to.',
+      'Drive like hell, you will get there.',
+      'Person who eat fortune cookie get lousy dessert.',
+      'Okay to look at past and future. Just don’t stare.',
+      'Wise person need either good manners or fast reflexes.',
+      'Soup was secret family recipe made from toad. Hope you liked!',
+      'You will soon have an out of money experience.',
+      'Probability of being seen directly proportional to stupidity of act.',
+      'He who dies with most toys, still dies.',
+      'Person who rests on laurels gets thorn in backside.',
+      'Practice safe eating. Always use condiments.',
+      'Person who give self haircut after rice wine will be buzzed.',
+      'Two can live as cheaply as one, for half as long.',
+      'Hard work pay off in future. Laziness pay off now.',
+      'Life is sexually transmitted condition.',
+      'Give person fish, he eat for day. Teach person to fish, he always smell funny.',
+      'Person who argue with idiot is taken for fool.',
+      'Look before you leap. Or wear parachute.',
+      'The end is near, might as well have dessert.',
+      'This fortune no good. Try another.',
+      'Of all 27 alternatives, running away is best.',
+      'Closed mouth gathers no feet.',
+    ];
+    const random = Math.floor(Math.random() * 30)
+
+    this.predictText = predictions[random];
+  }
+}
