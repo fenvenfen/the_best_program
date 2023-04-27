f20947cd56eadc23d20b7fc1eeaf6472f2f2879e
Bohdan <yulia.pakhachuk@gmail.com>
2023-04-27 14:42:58 +0200

    first simple components craeted

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WishCardComponent } from './wish-card/wish-card.component';
+import { GenerateBtnComponent } from './generate-btn/generate-btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WishCardComponent,
+    GenerateBtnComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/generate-btn/generate-btn.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generate-btn',
+  templateUrl: './generate-btn.component.html',
+  styleUrls: ['./generate-btn.component.css']
+})
+export class GenerateBtnComponent {
+
+}
