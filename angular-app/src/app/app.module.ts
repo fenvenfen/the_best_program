e54e8041bce50440b810edd95c8660dd2e8ae0e2
Katya <katyanikitina16@gmail.com>
2023-05-02 22:04:55 +0200

    Created input component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { InputComponent } from './input/input.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     NavBarComponent,
+    InputComponent,
   ],
   imports: [
     BrowserModule,
