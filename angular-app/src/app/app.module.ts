e4cdddf5a090d2e9834d172b136b1836d9cc11fb
Katya <katyanikitina16@gmail.com>
2023-05-02 21:02:25 +0200

    Created nav-bar component

--- angular-app/src/app/app.module.ts
+++ angular-app/src/app/app.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { LibraryComponent } from './library/library.component';
 import { HeaderComponent } from './header/header.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LibraryComponent,
     HeaderComponent,
+    NavBarComponent,
   ],
   imports: [
     BrowserModule,
