ce3b1b7e377adefc555598e5139693576815a0d6
Bohdan <yulia.pakhachuk@gmail.com>
2023-05-06 14:32:35 +0200

    refactor code

--- src/app/components/tags/tags.component.ts
+++ src/app/components/tags/tags.component.ts
@@ -1,54 +1,67 @@
-import { Component, OnInit, ViewChild, ViewChildren, ElementRef, QueryList } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  AfterViewInit,
+  ViewChild,
+  ViewChildren,
+  ElementRef,
+  QueryList,
+} from '@angular/core';
 import { TagsService } from 'src/app/tags.service';
 
 @Component({
   selector: 'app-tags',
   templateUrl: './tags.component.html',
   styleUrls: ['./tags.component.css'],
-  styles: [`
-    .opened {
-      transform: rotate(180deg);
-    }
-    .closed {
-      transform: rotate(0deg);
-    }
-    .hidden {
-      overflow: hidden;
-      height: 36px;
-    }
-  `],
-  providers: [TagsService]
+  styles: [
+    `
+      .opened {
+        transform: rotate(180deg);
+      }
+      .closed {
+        transform: rotate(0deg);
+      }
+      .hidden {
+        overflow: hidden;
+        height: 36px;
+      }
+    `,
+  ],
+  providers: [TagsService],
 })
-export class TagsComponent {
-  tags: {name: String}[] = [];
+export class TagsComponent implements AfterViewInit {
+  tags: { name: String }[] = [];
   isTagsOpened = false;
 
-  @ViewChild('taglist') taglist: ElementRef<any>;
-  
+  @ViewChild('taglist') taglist!: ElementRef;
+
   constructor(private tagsServece: TagsService) {
     this.taglist = {} as ElementRef;
   }
-  
+
   ngOnInit(): void {
     this.tags = this.tagsServece.tags;
   }
-  
+
   ngAfterViewInit(): void {
-    const tagsList = [...this.taglist.nativeElement.childNodes];
-    let commonWidth = 0;
-    const tagListGap = 4;
-    tagsList.forEach(e => {
-      if(e.offsetWidth) {
-        commonWidth = commonWidth + e.offsetWidth + tagListGap;
+    const tagsListChildren = [...this.taglist.nativeElement.childNodes];
+    const parentTagsWidth = this.taglist.nativeElement.offsetWidth;
+    this.checkTagsEnoughToOpenTaglist(tagsListChildren, parentTagsWidth)
+  }
+
+  checkTagsEnoughToOpenTaglist(tagsListChildren: { offsetWidth: number}[], parentTagsWidth: number){
+    const tagsWidth: number = tagsListChildren.reduce((commonWidth: number, tag) => {
+      const tagListGap = 4;
+      const { offsetWidth } = tag;
+      if(offsetWidth) {
+        commonWidth = commonWidth + offsetWidth + tagListGap;
         }
-      })
-      if(this.taglist.nativeElement.offsetWidth > commonWidth) {
-        this.isTagsOpened = true
-      }
+        return commonWidth;
+    }, 0);
+    this.isTagsOpened = parentTagsWidth > tagsWidth
   }
 
   toggleIsOpen() {
     this.isTagsOpened = !this.isTagsOpened;
   }
-  
 }
