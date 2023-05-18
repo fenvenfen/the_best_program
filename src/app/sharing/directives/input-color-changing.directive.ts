a40330c415930f61fcf0c5351c44ae8cb60b0f93
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2023-05-18 15:42:40 +0200

    finished

--- src/app/sharing/directives/input-color-changing.directive.ts
+++ src/app/sharing/directives/input-color-changing.directive.ts
@@ -8,7 +8,7 @@ export class InputColorChangingDirective {
 
   constructor(private el: ElementRef) { }
 
-  //чому використовуэться саме ОнЧендж лайфсайклХук?
+  //чому використовуэться саме ОнЧендж лайфсайкл Хук?
   ngOnChanges() {
     this.setBorderColor();
   }
