72246f56c98f9fd06d3183c1598fbc6bab3f4b18
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2023-05-18 15:41:23 +0200

    finished directive with ImgOrientation

--- src/app/sharing/directives/image-oriental-formating.directive.ts
+++ src/app/sharing/directives/image-oriental-formating.directive.ts
@@ -1,10 +1,38 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
   selector: '[appImageOrientalFormating]'
 })
 export class ImageOrientalFormatingDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) { }
 
+  ngAfterViewInit(): void {
+    const landScapeOrientation = this.el.nativeElement.width > this.el.nativeElement.height;
+    if(landScapeOrientation){
+      this.el.nativeElement.style.width = "100%";
+    } else {
+      this.el.nativeElement.style.height = "100%";
+      this.el.nativeElement.style.borderRadius = "10px";
+    }
+  }
 }
+
+
+// import { Directive, ElementRef, Renderer2 } from '@angular/core';
+
+// @Directive({
+//   selector: '[appImageSize]'
+// })
+// export class ImageSizeDirective {
+
+//   constructor(private el: ElementRef, private r: Renderer2) { 
+//   }
+
+//   ngAfterViewInit(): void {
+//     const isImgHorizontal = this.el.nativeElement.width > this.el.nativeElement.height;
+    
+//     if (isImgHorizontal) this.r.setStyle(this.el.nativeElement, 'width', '100%');
+//     else this.r.setStyle(this.el.nativeElement, 'height', '100%');
+//   }
+// }
