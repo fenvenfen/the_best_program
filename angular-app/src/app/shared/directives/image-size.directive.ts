2c3f334adaad30a861266a8a74c639e60ffd8500
Katya <katyanikitina16@gmail.com>
2023-05-22 11:00:50 +0200

    Corrected image size directive

--- angular-app/src/app/shared/directives/image-size.directive.ts
+++ angular-app/src/app/shared/directives/image-size.directive.ts
@@ -1,17 +1,19 @@
-import { Directive, ElementRef, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appImageSize]'
 })
-export class ImageSizeDirective {
+export class ImageSizeDirective implements OnInit {
 
   constructor(private el: ElementRef, private renderer: Renderer2) { }
 
   
-  ngAfterViewInit(): void {
-    const isImgHorizontal = this.el.nativeElement.width > this.el.nativeElement.height;
-    
-    if (isImgHorizontal) this.el.nativeElement.style.width = '100%';
-    else this.el.nativeElement.style.height = '100%';
+  ngOnInit(): void {
+    this.renderer.listen(this.el.nativeElement,'load', () => {
+      
+      const isImgHorizontal = this.el.nativeElement.width > this.el.nativeElement.height;
+      if (isImgHorizontal) this.el.nativeElement.style.width = '100%';
+      else this.el.nativeElement.style.height = '100%';
+    })
   }
 }
