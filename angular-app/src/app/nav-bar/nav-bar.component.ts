f0c9392ae50679c09e831b748ad3088329c704f9
Katya <katyanikitina16@gmail.com>
2023-05-24 13:08:55 +0200

    Added navigation to bread crumbs

--- angular-app/src/app/nav-bar/nav-bar.component.ts
+++ angular-app/src/app/nav-bar/nav-bar.component.ts
@@ -17,7 +17,7 @@ export class NavBarComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     this.routeSub = this.router.events.subscribe((event) => {
       if (event instanceof NavigationStart) {
-        this.currentRouteUrlArray = event.url.split('/').filter(n => n != '');
+        this.currentRouteUrlArray = event.url.split('/').filter(n => n != '' && n != 'home');
         this.getCurrentPage();
       }
     })
@@ -31,5 +31,11 @@ export class NavBarComponent implements OnInit, OnDestroy {
     if (this.currentRouteUrlArray.length) {
       this.currentPage = this.currentRouteUrlArray[this.currentRouteUrlArray.length - 1];
     } else this.currentPage = 'Home';
+  }
+
+  goToClickedRoute(route: string) {
+    const neededRouterLinkArray = this.currentRouteUrlArray.slice(0, this.currentRouteUrlArray.indexOf(route) + 1);
+    const neededRouterLink = `/${neededRouterLinkArray.join('/')}`;
+    this.router.navigate([neededRouterLink]);
   }
 }
