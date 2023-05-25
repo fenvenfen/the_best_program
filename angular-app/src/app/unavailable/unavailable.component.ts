8ab30642251c0672ba0a442eb6c6c332cda169d2
Katya <katyanikitina16@gmail.com>
2023-05-25 09:05:49 +0200

    Created goBack button for unavailable component

--- angular-app/src/app/unavailable/unavailable.component.ts
+++ angular-app/src/app/unavailable/unavailable.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-unavailable',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./unavailable.component.sass']
 })
 export class UnavailableComponent implements OnInit {
+  previousRouteUrl!: any;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.getPreviousRouteUrl();
   }
 
+  getPreviousRouteUrl() {
+    const currentRouteUrlArray = this.route.snapshot.url;
+    currentRouteUrlArray.pop();
+    this.previousRouteUrl = `/${currentRouteUrlArray.join('/')}`;
+  }
 }
