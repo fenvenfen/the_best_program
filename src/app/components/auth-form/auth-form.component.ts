cee4204a413fcf2c64968ceb1a8af31f7919d9dc
Yulia Pakhachuk <yulia.pakhachuk@gmail.com>
2023-06-28 01:04:21 +0200

    token and user localstor realized

--- src/app/components/auth-form/auth-form.component.ts
+++ src/app/components/auth-form/auth-form.component.ts
@@ -1,57 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { Observable } from 'rxjs';
+// import { Observable } from 'rxjs';
 import { Credentials, User } from 'src/app/interfaces';
+import { AuthenticationService } from 'src/app/servises/authentication.service';
 
 @Component({
   selector: 'app-auth-form',
   templateUrl: './auth-form.component.html',
-  styleUrls: ['./auth-form.component.css']
+  styleUrls: ['./auth-form.component.css'],
+  providers: [AuthenticationService],
 })
 export class AuthFormComponent implements OnInit {
   authForm!: FormGroup;
+
+  constructor(private authService: AuthenticationService) {}
 
   ngOnInit() {
     this.authForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
       name: new FormControl('', [Validators.required]),
     });
-    console.log(this.authForm);
   }
 
   submitForm() {
     console.log(this.authForm);
-    if (this.authForm.valid) {
-      console.log('Відправлено:', this.authForm.value);
+    if (!this.authForm.valid) {
+      return;
     }
-  }
 
-  login(credentials: Credentials): Observable<User | string>{
-    return new Observable((observer) => {
-        setTimeout(() => {
-            if (credentials.email === 'test@gmail.com'){
-                observer.next({
-                    name: 'Admin user',
-                    email: 'test@gmail.com',
-                    token: 'ikdjsaogiknk42tio3n2orhnol',
-                    admin: true
-                });
-            }else if (credentials.email === 'vova@gmail.com'){
-                observer.next({
-                    name: 'User',
-                    email: 'vova@gmail.com',
-                    token: 'ikdjdskljflkodsjfkldjslk',
-                    admin: false
-                });
-            }else{
-                observer.next('Wrong password');
-            }
-        }, 2000)
+    const user$ = this.authService.login(this.authForm.value);
+
+    user$.subscribe({
+      next: (value) => {
+        if (typeof value === 'string') {
+          alert(value);
+        } else {
+          this.authService.setCurrentUser(value);
+          this.authService.setToken(value.token);
+        }
+      },
     });
-    setTimeout(() => {
-            
-        }, 50000)
-}
-
+    // this.authService.login(this.authForm.value);
+  }
 
 }
